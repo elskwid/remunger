8929915b4873544dc5c15d2f90c5c088c7d06433
Don Morrison <don@elskwid.net>
2013-10-12 22:25:09 -0700

    Update remunger.rb to remove load path manipulation

--- lib/remunger.rb
+++ lib/remunger.rb
@@ -1,16 +1,13 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-require 'remunger/data'
-require 'remunger/report'
-require 'remunger/item'
-
-require 'remunger/render'
-require 'remunger/render/csv'
-require 'remunger/render/html'
-require 'remunger/render/sortable_html'
-require 'remunger/render/text'
-
 module Remunger
-  VERSION = '0.1.0'
+
 end
+
+require "remunger/data"
+require "remunger/item"
+require "remunger/render"
+require "remunger/render/csv"
+require "remunger/render/html"
+require "remunger/render/sortable_html"
+require "remunger/render/text"
+require "remunger/report"
+require "remunger/version"
